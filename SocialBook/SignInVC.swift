c3f12adbf78e52d1fbd2c10b669aaabe06a7bda3
Michael Chapman <submariner100@gmail.com>
2016-12-06 18:05:38 +0000

    Initial commit

---
+++ SocialBook/SignInVC.swift
@@ -0,0 +1,27 @@
+//
+//  SignInVC.swift
+//  SocialBook
+//
+//  Created by Macbook on 06/12/2016.
+//  Copyright © 2016 Chappy-App. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+     override func viewDidLoad() {
+          super.viewDidLoad()
+     
+     
+     
+     
+     
+     
+     
+     
+     }
+
+    
+}
+
