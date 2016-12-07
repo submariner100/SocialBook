672c8e53b0235de9b0619d4c1f70310f8344ebe8
Michael Chapman <submariner100@gmail.com>
2016-12-07 18:01:49 +0000

    facebook-auth

--- SocialBook/AppDelegate.swift
+++ SocialBook/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Firebase
+import FBSDKLoginKit
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,8 +21,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
           // Override point for customization after application launch.
           
           FIRApp.configure()
+          
+          FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+          
           return true
      }
+
 
      func applicationWillResignActive(_ application: UIApplication) {
           // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -42,6 +48,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
      func applicationWillTerminate(_ application: UIApplication) {
           // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+     }
+     
+     func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+          return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
+          
      }
 
 
--- SocialBook/SignInVC.swift
+++ SocialBook/SignInVC.swift
@@ -7,22 +7,50 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
 
 class SignInVC: UIViewController {
 
      override func viewDidLoad() {
           super.viewDidLoad()
                
-     
-     
-     
-     
-     
-     
-     
      }
 
-    
-}
+     @IBAction func facebookBtnTapped(_ sender: Any) {
+     
+          let facebookLogin = FBSDKLoginManager()
+          facebookLogin.logIn(withReadPermissions: ["email"], from: self) { ( result, error ) in
+          
+               if error != nil {
+                    print("Mike: Unable to auth with Facebook - \(error)")
+                    
+               } else if result?.isCancelled == true {
+                    
+                    print("Mike - user cancelled Facebook auth")
+                    
+               } else {
+                    
+                    print("Mike - Successfully auth with Facebook")
+                    
+                    let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                    self.firebaseAuth(credential)
+               }
+          }
+          
+     }
+     
+     func firebaseAuth(_ credential: FIRAuthCredential) {
+               FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+                    if error != nil {
+                         print("Mike - unable to authenicate with Firebase - \(error)")
+                         
+                    } else {
+                         print("Mike - Successfully authernicated with Firebase")
+                    }
+               })
+           }
+     }
 
 // Colors : #673AB7 #4527A0 415CA4
