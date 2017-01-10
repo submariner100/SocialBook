57f95109078deb910032b3fd1c475457b3682a49
Michael Chapman <submariner100@gmail.com>
2017-01-10 20:09:42 +0000

    completed Firebase users

--- SocialBook/SignInVC.swift
+++ SocialBook/SignInVC.swift
@@ -70,8 +70,10 @@ class SignInVC: UIViewController {
                     print("Mike - Successfully authenicated with Firebase")
                     
                     if let user = user {
+                    let userData = ["provider": credential.provider]
                     
-                    self.completeSignIn(id: user.uid)
+                    
+                    self.completeSignIn(id: user.uid, userData: userData)
                     
                     
                     }
@@ -89,7 +91,8 @@ class SignInVC: UIViewController {
                               print("Mike: Email user authenicated with Firebase")
                               
                               if let user = user {
-                              self.completeSignIn(id: user.uid)
+                              let userData = ["provider": user.providerID]
+                              self.completeSignIn(id: user.uid, userData: userData)
                               }
                               
                          } else {
@@ -101,7 +104,8 @@ class SignInVC: UIViewController {
                                         
                                    print("Mike: Successfully authenicate with Firebase")
                                    if let user = user {
-                                   self.completeSignIn(id: user.uid)
+                                   let userData = ["provider": user.providerID]
+                                   self.completeSignIn(id: user.uid, userData: userData)
                                    }
                                    }
                               })
@@ -113,7 +117,9 @@ class SignInVC: UIViewController {
           
                   }
      
-          func completeSignIn(id: String) {
+          func completeSignIn(id: String, userData: Dictionary<String, String>) {
+          
+               DataService.ds.createFirebaseDBUser(uid: id, userData: userData)
           
                let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
                //let keychainResult = KeychainWrapper.standard.string(forKey: KEY_UID)
